5bec7dedb2345051ec2aa858673a9a3989a44dce
noblejoshua73-dotcom <noblejoshua73@gmail.com>
2025-08-26 07:04:49 +0100

    Update update settings.js

--- update settings.js
+++ update settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'Proton', // Your name
-  ownerNumber: '09049123063', //Your number
+  ownerNumber: '+2347084946047', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
